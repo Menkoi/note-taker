fbc844e42114462b7627ef6322c64b6a9d00eb61
celaeon <celaeon@gmail.com>
2021-02-19 21:22:09 -0500

    added delete feature

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,37 +1,22 @@
-// Linking the noteContents in db to this routes.
-var noteContents = [
-    {
-    "title":"Test Title 1",
-    "text":"Test text",
-    "id":1
-    },
-    {
-    "title":"Test Title 2",
-    "text":"Test text",
-    "id":2
-    }
-    
-];
-
-//Create promise-based versions of functions using node style callbacks
 const fs = require("fs");
 const util = require("util");
+const router = require('express').Router();
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// Create a route
-module.exports = function(app) {
+var noteContents = require("../../db/noteContent");
 
-    //Display all notes
-    app.get("/api/notes", function(req, res) {
+
+    //display notes
+    router.get("/notes", function(req, res) {
         res.json(noteContents);
     });
 
-    //Create new posts
-    app.post("/api/notes", function(req, res) {
+    //create new posts
+    router.post("/notes", function(req, res) {
         
         let newNote = req.body;
 
-        // check to find last id in our notes json file, and assign the note to one greater than that id
+        // check to find last id, assign the note to one greater than that id
         let lastId = noteContents[noteContents.length - 1]["id"];
         let newId = lastId + 1;
         newNote["id"] = newId;
@@ -39,12 +24,37 @@ module.exports = function(app) {
         console.log("Req.body:", req.body);
         noteContents.push(newNote);
 
-        // write to the noteContents.json file as well
-        writeFileAsync( JSON.stringify(noteContents)).then(function() {
-            console.log("noteContents.json has been updated!");
+        writeFileAsync( "./db/db.json", JSON.stringify(noteContents))
+        .then(function() {
+            console.log("notes has been updated!");
         });
 
         res.json(newNote);
     });
+
+    // delete notes
+    router.delete("/notes/:id", function(req, res) {
+
+        console.log("Req.params:", req.params);
+        let chosenId = parseInt(req.params.id);
+        console.log(chosenId);
+
+
+        for (let i = 0; i < noteContents.length; i++) {
+            if (chosenId === noteContents[i].id) {
+                noteContents.splice(i,1);
+                
+                let noteJSON = JSON.stringify(noteContents, null, 1);
+                //placeholders for JSON
+                writeFileAsync("./db/db.json", noteJSON).then(function() {
+                console.log ("Chosen note has been deleted!");
+            });                 
+            }
+        }
+        res.json(noteContents);
         
-};
+    });
+
+    
+
+module.exports = router;
